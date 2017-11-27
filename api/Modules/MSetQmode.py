16ba21ddaf47ac4f098dad509750047ef5bb4e59
gtekelis <tekelisg@gmail.com>
2017-11-27 14:37:16 +0200

    first experiment ok

--- api/Modules/MSetQmode.py
+++ api/Modules/MSetQmode.py
@@ -5,20 +5,28 @@ def matSetQmode(field3):
     swb = Sunny_Web_Box2.SunnyWebBoxHTTP("147.102.30.65", password=pw)
     keySB3000 = "WRTM0U88:2130253427"
 
+    #data = -1
+
     def SetData(value):
         dat=swb.setParameter([{'key':keySB3000, 'channels':value}]);
 
-    if field3 == 0:
+    if field3 == '0':
         data = 'PFCnst'
-        #setData(data)
-    elif field3 == 1:
+        #sendFormValue(data)
+    elif field3 == '1':
         data = 'VArCtlVol'
-        #setData(data)
+        #sendFormValue(data)
 
     #data = field3 #"0.85" #sys.argv[1]
 
 
-
+    
+    #def sendFormValue( data ):
+    #    print "Set-point =" ,data
+    #    channels1 = [{'meta' : 'Q-VArMod', 'name' : 'Q-VArMod',   'value' : data}]
+    #    #channels1 = [{'meta' : 'PF-PF', 'name' : 'PF-PF',   'value' : data}]
+    #    SetData(channels1)
+    #    #return
           
     print "Set-point =" ,data
     channels1 = [{'meta' : 'Q-VArMod', 'name' : 'Q-VArMod',   'value' : data}]
